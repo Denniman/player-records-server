adeb422780c2933f085bc3be1288e13c6ffb2e89
Dennis <dennimanadams@gmail.com>
2021-05-18 22:40:07 +0100

    remove db in index file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const router = require('./routes/app')
-const db = require('./db/db')
+
 const app = express()
 const port = 3000
 
